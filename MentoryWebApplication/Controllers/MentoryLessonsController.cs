5f9c42554c4202d401ae3ca7c89663e3042b6e53
Olzhas Zait <zait.olzhas@gmail.com>
2024-01-30 18:56:56 +0600

    Add project files.

---
+++ MentoryWebApplication/Controllers/MentoryLessonsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MentoryWebApplication.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MentoryLessonsController: ControllerBase
+    {
+        [HttpGet(Name = "GetGroupList")]
+        public IEnumerable<string> Get()
+        {
+            return new List<string> { "Group 1", "Group 2", "Group 3" };        
+        }
+    }
+}
